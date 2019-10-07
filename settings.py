b2ef41fcead132a95f970e037026b308f46e37dc
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2019-10-07 21:49:24 +0545

    Changes the default settings values

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@
 REGION = 'EUW'
 LOCALE = 'en_GB'
 
-RIOT_CLIENT_SERVICES_PATH = 'D:/Games/Riot Games/Riot Client/RiotClientServices.exe'
-LEAGUE_CLIENT_PATH = 'D:/Games/League of Legends/LeagueClient.exe'
+RIOT_CLIENT_SERVICES_PATH = 'E:/Riot Games/Riot Client/RiotClientServices.exe'
+LEAGUE_CLIENT_PATH = 'E:/Riot Games/League of Legends/LeagueClient.exe'
 LEAGUE_CLIENT_PROCESS = 'LeagueClient.exe'
 RIOT_CLIENT_PROCESS = 'RiotClientServices.exe'
 RIOT_CLIENT_CONFIG = '~/AppData/Local/Riot Games/Riot Client/Config'
